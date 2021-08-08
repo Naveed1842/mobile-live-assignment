7a1ba9354b7c121bec98e028c5c4789a0f0976d7
naveed1842 <naveed4034@gmail.com>
2021-08-08 06:12:37 +0500

    output event added to child component

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+interface userInfo {
+  name: string;
+  age: number;
+  gender: string;
+}
 @Component({
   selector: 'user-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.css']
+  styleUrls: ['./user-create.component.css'],
 })
 export class UserCreateComponent implements OnInit {
   form: FormGroup;
-  genderList: any = ['Male', 'Female'];
 
-  constructor() { 
+  genderList: string[] = ['Male', 'Female'];
+  @Output() newItemEvent = new EventEmitter<userInfo>();
+  constructor() {
     this.form = new FormGroup({
-      username: new FormControl(),
+      name: new FormControl(),
       age: new FormControl(),
-      gender: new FormControl()
+      gender: new FormControl(),
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  changeCity(event:any){
-    console.log(event)
-  }
-
-  onSubmit(){
-
+  onSubmit() {
+    if (this.form.valid) {
+      const { name, age, gender } = this.form.value;
+      const userInfo: userInfo = {
+        name: name,
+        age: age,
+        gender: gender,
+      };
+      this.newItemEvent.emit(userInfo);
+      this.form.reset();
+    }
   }
 }
--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+interface userInfo {
+  name: string;
+  age: number;
+  gender: string;
+}
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
-  users = [
-    { name: 'Ali', age: 'Ahmed', gender: 'Male' },
-    { name: 'Fatima', age: 'Bin', gender: 'Female' },
-    { name: 'Amna', age: 'Shakeel', gender: 'Female' },
+  users:userInfo[] = [
+    { name: 'Ali', age: 12, gender: 'Male' },
+    { name: 'Fatima', age: 15, gender: 'Female' },
+    { name: 'Amna', age: 25, gender: 'Female' },
   ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  addUserIntoList(item: userInfo) {
+    this.users.push(item);
+  }
 }
