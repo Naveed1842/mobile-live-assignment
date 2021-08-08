6e1438bd0fe531c52884565870f394d67c26d106
Muhammad Naved <naveed4034@gmail.com>
2021-08-08 07:49:55 +0500

    Update calculate-standard-deviation.component.ts

--- src/app/calculate-standard-deviation/calculate-standard-deviation.component.ts
+++ src/app/calculate-standard-deviation/calculate-standard-deviation.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculate-standard-deviation.component.css']
 })
 export class CalculateStandardDeviationComponent implements OnInit {
-
+  dataString;
+  dataError;
+  answer: any;
+  data = [];
+  dataInput:any=[];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  converStringToData() {
+    let valid = false;
+    if(this.dataString){
+      if (this.dataString.length > 0) {
+      this.data = this.dataString.trim().split(',');
+      this.data.forEach((itm, index) => {
+        if (itm) {
+          this.data[index] = parseFloat(this.data[index]);
+          valid = true;
+        } else {
+          this.dataError = "Invalid data";
+          valid = false;
+        }
+      })
+      return valid
+    } else {
+      this.dataError = "No data provided";
+      return false;
+    }
+    }else{
+      this.dataError = "No data provided";
+      return false;
+    }
+  }
+  calculateSD(){
+    if(this.converStringToData()){
+      this.dataInput=[];
+       this.dataInput = this.data;
+    }
   }
 
 }
