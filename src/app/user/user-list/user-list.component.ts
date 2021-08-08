7fd5ee52f471c0f724435d87dc03253dabda3f98
naveed1842 <naveed4034@gmail.com>
2021-08-08 05:41:05 +0500

    username list modified

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -3,17 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
   users = [
-    { name: 'Ali', age: 'Ahmed', gender: 'male' },
-    { name: 'Vic', age: 'Reynolds', gender: 'male' },
-    { name: 'Gina', age: 'Jabowski', gender: 'female' }
-];
-  constructor() { }
+    { name: 'Ali', age: 'Ahmed', gender: 'Male' },
+    { name: 'Fatima', age: 'Bin', gender: 'Female' },
+    { name: 'Amna', age: 'Shakeel', gender: 'Female' },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
